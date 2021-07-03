e012d30c36862f5f02636e53e769b4002bf6974c
Spirit <omatsolaseund@gmail.com>
2021-07-03 06:10:05 +0100

    assignment on multi page node server done

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((Request, Response)  =>{
+    Response.statusCode = 200;
+    if (Request.url === '/') {
+        Response.setHeader('Content-Type', 'text/html');
+                fs.readFile('./pages/index.html', (err,data)=>{
+                Response.end(data);
+                })
+                
+    }    
+    else if (Request.url === '/index.html') {
+            Response.setHeader('Content-Type', 'text/html');
+            fs.readFile('./pages/index.html', (err,data)=>{
+            Response.end(data);
+            })
+        }
+    else if (Request.url === '/contact.html') {
+            Response.setHeader('Content-Type', 'text/html');
+            fs.readFile('./pages/contact.html', (err,data)=>{
+            Response.end(data);
+            })
+        }
+    else if (Request.url === '/about.html') {
+            Response.setHeader('Content-Type', 'text/html');
+            fs.readFile('./pages/about.html', (err,data)=>{
+                Response.end(data);
+            })
+        }
+    else {
+            Response.setHeader('Content-Type', 'text/html');
+            fs.readFile('./pages/error.html', (err,data)=>{
+            Response.end(data);
+            })
+        }
+    console.log(Request.url);
+}).listen(port, hostname, ()=>{
+    console.log(`Server running at http://${hostname}:${port}`)
+    
+});
